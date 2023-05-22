3d0561f89cd4caaea58c5816b85d95ff3b9b3e94
Shashwat Shah <shashwatshah08@yahoo.com>
2023-05-21 23:29:51 -0300

    remove mentions, hashtags and urls from the text

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
-from preprocess_data import remove_unwanted_columns
+from preprocess_data import preprocess_data
 
 if "__main__" == __name__:
+    print("loading data...")
     df = pd.read_csv(
         "./archive/training.1600000.processed.noemoticon.csv", encoding="ISO-8859-1"
     )
-    df = remove_unwanted_columns(df)
+    print("data loaded")
+    print("preprocessing data...")
+    df = preprocess_data(df)
+    print("data preprocessed")
     print(df.head())
--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,4 +1,22 @@
+import re
+
+
+def preprocess_data(df):
+    df = remove_unwanted_columns(df)
+    df["text"] = df["text"].apply(preprocess_text)
+    return df
+
+
 def remove_unwanted_columns(df):
     df.columns = ["target", "ids", "date", "flag", "user", "text"]
     df = df.drop(["ids", "date", "flag", "user"], axis=1)
     return df
+
+
+def preprocess_text(text):
+    text = re.sub(r"http\S+|www\S+|https\S+", "", text)
+    text = re.sub(r"@[A-Za-z0-9]+", "", text)
+    text = re.sub(r"#", "", text)
+    text = re.sub(r"[^A-Za-z ]+", "", text)
+    text = text.lower()
+    return text
