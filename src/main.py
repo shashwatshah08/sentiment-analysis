edad6d542d77c06f1884b5a6bd6923aff86e53f7
Shashwat Shah <shashwatshah08@yahoo.com>
2023-05-22 23:20:56 -0300

    evaluation report created

--- src/main.py
+++ src/main.py
@@ -3,30 +3,32 @@ import time
 import pandas as pd
 
 from preprocess_data import preprocess_data
+from src.evaluate_model import evaluate_model
 from src.feature_extraction import feature_extraction
 from src.save_model import save_model, load_model
 from src.split_dataset import split_dataset
 from src.train_model import train_model
 
 if "__main__" == __name__:
+
+    print("loading data...")
+    df = pd.read_csv(
+        "../archive/training.1600000.processed.noemoticon.csv", encoding="ISO-8859-1"
+    )
+    print("data loaded")
+    print("preprocessing data...")
+    df = preprocess_data(df)
+    print("data preprocessed")
+    print("creating feature matrix...")
+    matrix = feature_extraction(df)
+    print("feature matrix created")
+    print("splitting dataset...")
+    x_train, x_test, y_train, y_test = split_dataset(matrix, df["target"])
+    print("dataset splitted")
     print("loading model...")
     model = load_model()
     if not model:
         print("model not found")
-        print("loading data...")
-        df = pd.read_csv(
-            "../archive/training.1600000.processed.noemoticon.csv", encoding="ISO-8859-1"
-        )
-        print("data loaded")
-        print("preprocessing data...")
-        df = preprocess_data(df)
-        print("data preprocessed")
-        print("creating feature matrix...")
-        matrix = feature_extraction(df)
-        print("feature matrix created")
-        print("splitting dataset...")
-        x_train, x_test, y_train, y_test = split_dataset(matrix, df["target"])
-        print("dataset splitted")
         print("training model...")
         start = time.time()
         model = train_model(x_train, y_train)
@@ -37,3 +39,8 @@ if "__main__" == __name__:
         print("model saved")
     else:
         print("model loaded")
+    print("evaluating model...")
+    report = evaluate_model(model, x_test, y_test)
+    print("model evaluated")
+    print("report:")
+    print(report)
